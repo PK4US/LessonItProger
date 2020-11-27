b9a7a1d415343fe06527e11a81396bc445d651a6
PK4U_S <stepanchuk.ei@gmail.com>
2020-11-28 00:06:06 +0200

    new intent

--- lesson_02/src/main/java/com/pk4u/lesson_02/MainActivity.java
+++ lesson_02/src/main/java/com/pk4u/lesson_02/MainActivity.java
@@ -1,8 +1,10 @@
 package com.pk4u.lesson_02;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void OnBtnClick(View v) {
+        EditText text_from_user = (EditText)findViewById(R.id.editText);
+        TextView result = (TextView)findViewById(R.id.result_field);
+
+        String text = text_from_user.getText().toString();
+        if (!text.matches("")) {
+            float num = Float.parseFloat(text);
+            num *= 1.61;
+            result.setText(Float.toString(num));
+        }
     }
 }
